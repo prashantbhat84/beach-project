b48c8113b3db08b0a2887da7982c4112bab66d9c
prashant-freelance <prashant.work.1984@gmail.com>
2020-01-24 16:27:49 +0530

    Updates

---
+++ precache-manifest.2cfdfa3d801c97b60bff6a2c0c56b385.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd3f4f9a8fd86192d79e84aa8234898",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fc85ec5324935bb2fa8",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9bec0b018c10190bfbf2",
+    "url": "/static/js/2.09c236c9.chunk.js"
+  },
+  {
+    "revision": "9fc85ec5324935bb2fa8",
+    "url": "/static/js/main.06a3827a.chunk.js"
+  },
+  {
+    "revision": "5b512e511a1ce80175ad",
+    "url": "/static/js/runtime-main.104bd7b2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
